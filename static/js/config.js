b55b7c657d76140268fa1d18a51828d38ca04a55
PlainJane20 <72418851+PlainJane20@users.noreply.github.com>
2021-02-19 22:12:35 -0800

    updated api

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API Key
-const API_KEY = 'pk.eyJ1IjoicGxhaW5qYW5lMjAiLCJhIjoiY2tsZDVlMDB4MzFxNzJucGVha3RnejNsbCJ9.XyUZmod9iumL8yTl7l8P6A';
+const API_KEY = "pk.eyJ1IjoicGxhaW5qYW5lMjAiLCJhIjoiY2tsZGJveHVnMzNoejJucGU1OGFqcDNudyJ9.E-A5E1ru8MN8hs4Wad-9mA";
