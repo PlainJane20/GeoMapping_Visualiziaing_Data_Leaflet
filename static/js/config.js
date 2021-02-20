7c87c9cb889653e2347b9f98b5a69c5a088d41e7
PlainJane20 <72418851+PlainJane20@users.noreply.github.com>
2021-02-19 22:07:30 -0800

    updates to logic

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,2 @@
-//API key
-var config = {
-
-API_KEY:'pk.eyJ1IjoicGxhaW5qYW5lMjAiLCJhIjoiY2tsZDVlMDB4MzFxNzJucGVha3RnejNsbCJ9.XyUZmod9iumL8yTl7l8P6A'
-
-}
+// API Key
+const API_KEY = 'pk.eyJ1IjoicGxhaW5qYW5lMjAiLCJhIjoiY2tsZDVlMDB4MzFxNzJucGVha3RnejNsbCJ9.XyUZmod9iumL8yTl7l8P6A';
